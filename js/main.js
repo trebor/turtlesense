ec97ab8ed554bff559aeed5ea59a9fdb1a605933
Robert Harris <rharris@twitter.com>
2014-09-01 13:58:10 -0700

    wire up whith require

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("hello, world! you look amazing!");
+requirejs.config({
+    baseUrl: 'bower_components',
+    paths: {
+        d3: 'd3/d3.min'
+    }
+});
+
+// app starts here
+
+define(['d3'], function (d3) {
+  console.log("d3", d3);
+  console.log("hello, world! you look amazing!");
+});
+
