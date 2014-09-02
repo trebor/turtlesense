86b11ffb5495ee07dd86beaa09e24e13db4e4ffd
Robert Harris <rharris@twitter.com>
2014-09-02 10:56:05 -0700

    fix bootstrap js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 requirejs.config({
-    baseUrl: 'bower_components',
-    paths: {
-        d3: 'd3/d3.min'
-    }
+  baseUrl: 'bower_components',
+  paths: {
+    d3: 'd3/d3.min',
+    jquery: 'jquery/dist/jquery.min',
+    bootstrap: 'bootstrap/dist/js/bootstrap.min',
+  },
+  shim: {
+    bootstrap: {deps: ['jquery']},
+  }    
 });
 
-define(['d3'], function (d3) {
-  console.log("d3 the new", d3);
+define(['d3', 'jquery', 'bootstrap'], function (d3, $, bs) {
+  console.log("d3", d3);
   console.log("hello, world! you look amazing!");
 });
 
