5340fbd5434e9807edfd42451b3459ea1f91af97
Robert Harris <rharris@twitter.com>
2015-08-22 15:34:40 -0700

    fix quotes

--- js/components/nestchart.js
+++ js/components/nestchart.js
@@ -1,4 +1,4 @@
-define(["d3", "lodash", "baseChart"], function(d3, _, BaseChart) {
+define(['d3', 'lodash', 'baseChart'], function(d3, _, BaseChart) {
 
 // base svg chart, which auto resizes to fit containing element
 
@@ -19,14 +19,14 @@ var module = function($chartNode, customOptions, extendedEvents) {
   var xAxis = d3.svg.axis()
     .scale(x)
     .ticks(4)
-    .tickFormat(d3.time.format("%b %e"))
-    .orient("bottom");
+    .tickFormat(d3.time.format('%b %e'))
+    .orient('bottom');
   var yTmpAxis = d3.svg.axis()
     .scale(yTmp)
-    .orient("left");
+    .orient('left');
   var yNrgAxis = d3.svg.axis()
     .scale(yNrg)
-    .orient("right");
+    .orient('right');
 
   var tempLine = d3.svg.line()
     .x(function(d) { return x(d.date);})
@@ -41,28 +41,28 @@ var module = function($chartNode, customOptions, extendedEvents) {
     onResize(baseChart.getDimensions());
 
     svg = baseChart.getContainer()
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + (dimensions.height - margin.bottom) + ")")
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + (dimensions.height - margin.bottom) + ')')
       .call(xAxis);
 
-    svg.append("g")
-      .attr("class", "yTmp axis")
+    svg.append('g')
+      .attr('class', 'yTmp axis')
       .call(yTmpAxis);
 
-    svg.append("g")
-      .attr("transform", "translate(" + [dimensions.width - margin.right , 0] + ")")
-      .attr("class", "yNrg axis")
+    svg.append('g')
+      .attr('transform', 'translate(' + [dimensions.width - margin.right , 0] + ')')
+      .attr('class', 'yNrg axis')
       .call(yTmpAxis);
 
-    svg.append("path")
-      .classed("temperature", true);
+    svg.append('path')
+      .classed('temperature', true);
 
-    svg.append("path")
-      .classed("energyLow", true);
+    svg.append('path')
+      .classed('energyLow', true);
   }
 
   function setNest(nest) {
@@ -70,19 +70,19 @@ var module = function($chartNode, customOptions, extendedEvents) {
     yTmp.domain(d3.extent(nest.samples, function(d) { return d.temperature; }));
     yNrg.domain([d3.min(nest.samples, function(d) { return d.energyLow; }), 20]);
 
-    d3.select("path.temperature")
+    d3.select('path.temperature')
       .datum(nest.samples)
-      .attr("d", tempLine);
+      .attr('d', tempLine);
 
-    d3.select("path.energyLow")
+    d3.select('path.energyLow')
       .datum(nest.samples)
-      .attr("d", nrgLine);
+      .attr('d', nrgLine);
 
-    d3.select(".yTmp.axis")
+    d3.select('.yTmp.axis')
       .call(yTmpAxis);
-    d3.select(".yNrg.axis")
+    d3.select('.yNrg.axis')
       .call(yNrgAxis);
-    d3.select(".x.axis")
+    d3.select('.x.axis')
       .call(xAxis);
   }
 
